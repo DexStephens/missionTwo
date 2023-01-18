0675faf3803e5517c210e46a9348a992c85e0237
Dexter Stephens <dexterfstephens@gmail.com>
2023-01-18 08:32:15 -0700

    Add comments as well as a clarification that the user needs to enter a number greater than 0

--- missionTwo/DiceRoller.cs
+++ missionTwo/DiceRoller.cs
@@ -7,6 +7,7 @@ namespace missionTwo
 {
     public class DiceRoller
     {
+        //Instance variables include the rolls specified and a dictionary that holds all of the percentages
         public int Rolls { get; set; }
         public Dictionary<int, string> TotalPercents = new Dictionary<int, string>(){ { 2, "" }, { 3, "" }, { 4, "" }, { 5, "" }, { 6, "" }, { 7, "" }, { 8, "" }, { 9, "" }, { 10, "" }, { 11, "" }, { 12, "" } };
 
@@ -14,7 +15,9 @@ namespace missionTwo
         {
             Rolls = rolls;
         }
-
+        /// <summary>
+        /// This method will loop through the specified number of rolls and then call the calculate percents method
+        /// </summary>
         public void RollDice()
         {
             Dictionary<int, int> totalRolls = new Dictionary<int, int>() { { 2, 0 }, { 3, 0 }, { 4, 0 }, { 5, 0 }, { 6, 0 }, { 7, 0 }, { 8, 0 }, { 9, 0 }, { 10, 0 }, { 11, 0 }, { 12, 0 } };
@@ -28,7 +31,10 @@ namespace missionTwo
             }
             CalculatePercents(totalRolls);
         }
-
+        /// <summary>
+        /// This function will accept the dictionary of total rolls for each value and then add the specified percent to the TotalPercents dictionary
+        /// </summary>
+        /// <param name="totalRolls"></param>
         public void CalculatePercents(Dictionary<int, int> totalRolls)
         {
             foreach(int total in totalRolls.Keys)
--- missionTwo/Program.cs
+++ missionTwo/Program.cs
@@ -6,9 +6,11 @@ namespace missionTwo
     {
         static void Main(string[] args)
         {
+            //Ask the user how many times they would like to roll the dice
             Console.WriteLine("Welcome to the dice throwing simulator!\n");
             Console.WriteLine("How many dice rolls would you like to simulate?");
             int rolls = 0;
+            //Make sure the user inputs a valid number that is greater than 0
             while (rolls == 0)
             {
                 try
@@ -17,18 +19,21 @@ namespace missionTwo
                 }
                 catch
                 {
-                    Console.WriteLine("Please enter a valid number");
+                    Console.WriteLine("Please enter a valid number greater than 0");
                 }
             }
 
+            //Instantiate a new instance of the DiceRoller class
             DiceRoller dr = new DiceRoller(rolls);
 
             Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
             Console.WriteLine("Each '*' represents 1% of the total number of rolls.");
             Console.WriteLine($"Total number of rolls = {dr.Rolls}\n");
 
+            //Call the method that will roll the dice and calculate the number
             dr.RollDice();
 
+            //Looop through the dictionary keys and output the *** representing the total percentage
             foreach(int total in dr.TotalPercents.Keys)
             {
                 Console.WriteLine($"{total}: {dr.TotalPercents[total]}");
